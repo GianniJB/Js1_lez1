1783db7819345175cdaad59b17613bcdca9757af
Gianni B. <gianni.bascetta@gmail.com>
2024-05-10 00:03:51 +0200

    finalFix

--- D1.js
+++ D1.js
@@ -25,18 +25,23 @@ Booleani. Quando vogliamo fare capire al nostro codice che in quel contenitore p
  Crea una variable chiamata "myName" e assegna ad essa il tuo nome, sotto forma di stringa.
 */
 
-const myName = "Gianni";
+let myName = "Gianni";
+
+console.log("Il mio nome è:", myName);
 
 /* ESERCIZIO 3
  Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
 */
 
-/*SOLUZIONE ALTERNATIVA
-  const numero1 = 12
-  const numero2 = 20
-*/
+const num1 = 12;
+const num2 = 20;
 
-console.log(12 + 20);
+console.log("prima variabile numero:", num1);
+console.log("seconda variabile numero:", num2);
+
+let num3 = num1 + num2;
+
+console.log("somma di 2 variabili numero in una terza:", num3);
 
 /* ESERCIZIO 4
  Crea una variable di nome "x" e assegna ad essa il numero 12.
@@ -44,23 +49,22 @@ console.log(12 + 20);
 
 const x = 12;
 
-console.log(x);
+console.log("La variabile x è:", x);
 
 /* ESERCIZIO 5
   Riassegna un nuovo valore alla variabile "myName" già esistente: il tuo cognome.
   Dimostra l'impossibilità di riassegnare un valore ad una variabile dichiarata con il costrutto const.
 */
 
-// myName = "Bascetta"
-// disattivare il commento superiore per verificare
+myName = "Bascetta";
 
-console.log(myName);
+console.log("Funziona perchè ho scritto la variabile con let:", myName);
 
 /* ESERCIZIO 6
  Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
 */
-
-console.log(4 - x);
+let sottrazione = 4 - x;
+console.log("4 - x è uguale a:", sottrazione);
 
 /* ESERCIZIO 7
  Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
@@ -70,9 +74,10 @@ console.log(4 - x);
 
 const name1 = "john";
 const name2 = "John";
+const different = name1 !== name2;
 
-console.log(name1 !== name2);
+console.log("Sono diversi?:", different);
 
 // Extra
-
-console.log(name1 === name2.toLowerCase());
+const areSame = name1.toLowerCase === name2.toLowerCase;
+console.log("Sono uguali?", areSame);
